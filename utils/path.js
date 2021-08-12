1fd66c9bea64c81391d9b780f476d852e13b2282
flyc <yayayahahahaooii@gmail.com>
2021-08-12 21:39:31 +0800

    implement path.js

--- utils/path.js
+++ utils/path.js
@@ -1,6 +1,9 @@
 import fs from 'fs'
 
 const checkFolder = (dir = './') => {
+  const last = dir.split('/').pop()
+  if (last.match(/\./g)) return void console.error('路徑裡除了當前路徑以外不可以有 . ')
+
   if (fs.existsSync(dir)) return
   fs.mkdirSync(dir, { recursive: true })
 }
